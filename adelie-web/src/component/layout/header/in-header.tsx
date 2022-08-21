e71830908db4840933179b5aa2079d6c769415f2
kwon <jazzjack@kwonui-MacBookPro.local>
2022-08-22 00:22:18 +0900

    clean

--- adelie-web/src/component/layout/header/in-header.tsx
+++ adelie-web/src/component/layout/header/in-header.tsx
@@ -1,16 +1,20 @@
-import {Image, Menu, MenuItem} from 'semantic-ui-react';
+import {Image} from 'semantic-ui-react';
 import './in-header.less';
-import {NavLink} from "react-router-dom";
+import {Link} from "react-router-dom";
 import React from 'react';
 
 const InHeader: React.FC = () => {
     return (
-        <Menu fixed='top' inverted>
-            <MenuItem as={NavLink} to="/">
+        <div className="InHeader">
+            <Link to="/">
                 <Image size='mini' src='/logo192.png' style={{marginRight: '1.5em'}}/>Home
-            </MenuItem>
-            <MenuItem as={NavLink} to="/project">Manage Project</MenuItem>
-        </Menu>);
+            </Link>
+            <Link to="/project">
+                Manage Project
+            </Link>
+
+
+        </div>);
 }
 
 /* Exports */
